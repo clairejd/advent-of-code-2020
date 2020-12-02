a08d818a43e747f23ffdb752f14fdb187d15e864
claire <clairedurant322@gmail.com>
2020-12-01 23:07:05 -0500

    finished day 1 (but not too proud of it...)

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
 use std::io;
-// for report_repair (todo: remove)
-use std::fs;
+use std::io::Read;
+use std::fs::File;
 
 fn main() {
     println!("Advent of Code 2020 runner");
     println!("Enter the number of the program you'd like to run.");
     
-    let mut program_was_run = false;
+    // get program number as a string from user
+    let mut prog_number = String::new();
+    io::stdin()
+        .read_line(&mut prog_number)
+        .expect("Failed to read line");
 
-    while !(program_was_run) {        
-        // get program number as a string from user
-        let mut prog_number = String::new();
-        io::stdin()
-            .read_line(&mut prog_number)
-            .expect("Failed to read line");
-
-        let prog_number: u32 = match prog_number.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Not a number.");
-                continue
-            },
-        };
+    // todo: improve error checking
+    let prog_number = prog_number
+        .trim()
+        .parse()
+        .expect("not a number!");
         
-        // now we know we have a valid program number, run the program
-        // if the program does not exist, run_program returns false
-        let program_was_run = match run_program(prog_number);
-    }
+    run_program(prog_number);
 }
 
 /******************************************************************/
@@ -34,10 +26,10 @@ fn main() {
 /* Returns true if program was run, returns false if no such      */
 /*   program exists.                                              */
 /******************************************************************/
-fn run_program(prog_number: u32) -> i32 {
+fn run_program(prog_number: u32) {
     match prog_number {
-        1 => report_repair();
-        _ => println!("Not a valid program number");
+        1 => report_repair(),
+        _ => println!("Not a valid program number"),
     }
 }
 
@@ -45,7 +37,8 @@ fn run_program(prog_number: u32) -> i32 {
 /* TODO: split into its own file                      */
 /******************************************************/
 fn report_repair() {
-    let mut file = std::fs::File::open("inputs/1.txt").unwrap();
+    // todo: error handling for missing file
+    let mut file = File::open("inputs/1.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     
@@ -54,6 +47,28 @@ fn report_repair() {
         numbers.push(s.parse::<i32>().unwrap());
     }
 
-    // now we have a vector containing each number in the file
+    // sort the vector
+    numbers.sort();
     
+    // simple, stupid O(n^3) solution
+    // maintain two iterators, start both at the beginning of the list
+    // add each pair of numbers together until you find the answer
+    for low_num in numbers.iter() {
+        let mut done = false;
+        for high_num in numbers.iter() {
+            for mid_num in numbers.iter() {
+                if low_num + mid_num + high_num == 2020 {
+                    println!("found it! {}", low_num * mid_num * high_num);
+                    done = true;
+                    break;
+                }
+            }
+            if done {
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
 }
