dbc67c68c24d3b58d798f493c8a401742efc7f45
Claire Durant <clairedurant322@gmail.com>
2020-12-02 21:48:54 -0500

    moving to another computer

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,27 @@
-use std::io;
-use std::io::Read;
-use std::fs::File;
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+//use std::io;
+//use std::io::Read;
+//use std::fs::File;
+
+mod report_repair;
 
 fn main() {
-    println!("Advent of Code 2020 runner");
-    println!("Enter the number of the program you'd like to run.");
-    
-    // get program number as a string from user
-    let mut prog_number = String::new();
-    io::stdin()
-        .read_line(&mut prog_number)
-        .expect("Failed to read line");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
-    // todo: improve error checking
-    let prog_number = prog_number
-        .trim()
-        .parse()
-        .expect("not a number!");
-        
-    run_program(prog_number);
-}
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 
-/******************************************************************/
-/* Runs a program based on the program number.                    */
-/* Returns true if program was run, returns false if no such      */
-/*   program exists.                                              */
-/******************************************************************/
-fn run_program(prog_number: u32) {
-    match prog_number {
-        1 => report_repair(),
-        _ => println!("Not a valid program number"),
-    }
-}
-
-/******************************************************/
-/* TODO: split into its own file                      */
-/******************************************************/
-fn report_repair() {
-    // todo: error handling for missing file
-    let mut file = File::open("inputs/1.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    
-    let mut numbers: Vec<i32> = Vec::new();
-    for s in contents.lines() {
-        numbers.push(s.parse::<i32>().unwrap());
-    }
-
-    // sort the vector
-    numbers.sort();
-    
-    // simple, stupid O(n^3) solution
-    // maintain two iterators, start both at the beginning of the list
-    // add each pair of numbers together until you find the answer
-    for low_num in numbers.iter() {
-        let mut done = false;
-        for high_num in numbers.iter() {
-            for mid_num in numbers.iter() {
-                if low_num + mid_num + high_num == 2020 {
-                    println!("found it! {}", low_num * mid_num * high_num);
-                    done = true;
-                    break;
-                }
-            }
-            if done {
-                break;
-            }
-        }
-        if done {
-            break;
-        }
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
     }
 }
---
+++ src/report_repair.rs
@@ -0,0 +1,39 @@
+/*use std::io::prelude::*;
+use std::fs::File;
+
+pub fn report_repair() {
+    // todo: error handling for missing file
+    let mut file = File::open("inputs/1.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+        
+    let mut numbers: Vec<i32> = Vec::new();
+    for s in contents.lines() {
+        numbers.push(s.parse::<i32>().unwrap());
+    }
+
+    // sort the vector
+    numbers.sort();
+        
+    // simple, stupid O(n^3) solution
+    // maintain two iterators, start both at the beginning of the list
+    // add each pair of numbers together until you find the answer
+    for low_num in numbers.iter() {
+        let mut done = false;
+        for high_num in numbers.iter() {
+            for mid_num in numbers.iter() {
+                if low_num + mid_num + high_num == 2020 {
+                    println!("found it! {}", low_num * mid_num * high_num);
+                    done = true;
+                    break;
+                }
+            }
+            if done {
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
+}*/
